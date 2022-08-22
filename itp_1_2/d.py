c67f7b569b694830fa9e2c6e907a6c34f2c5d9cc
yujin <deus1@outlook.jp>
2022-08-22 11:15:19 +0900

    circle in a rectangle

--- itp_1_2/d.py
+++ itp_1_2/d.py
@@ -1,3 +1,12 @@
+W, H, x, y, r = map(int, input().split())
+
+if 0 <= x-r and x+r <= W and 0 <= y-r and y+r <= H:
+    print('Yes')
+else:
+    print('No')
+
+
+"""
 w, h, x, y, r = map(int, input().split())
 
 if (x - r) >= 0 and (y - r) >= 0:
@@ -7,3 +16,4 @@ if (x - r) >= 0 and (y - r) >= 0:
         print("No")
 else:
     print("No")
+"""
